82b52340966159f53172390d417447a1588f948d
Beru003 <bernardsaif01@gmail.com>
2024-07-07 17:07:52 +0700

    detail

--- demo/src/main/java/com/example/LaporanMslhController.java
+++ demo/src/main/java/com/example/LaporanMslhController.java
@@ -39,6 +39,9 @@ public class LaporanMslhController {
     @FXML
     private Button deleteButton;
 
+    @FXML
+    private Button lanjutbButton;
+
     private ObservableList<Issue> issueData = FXCollections.observableArrayList();
 
     @FXML
